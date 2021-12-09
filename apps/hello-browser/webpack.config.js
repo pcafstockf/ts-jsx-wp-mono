40ffab6f0ac6e2b5899f19406596af3d69954800
pcafstockf <github@bytelightning.com>
2021-12-09 14:36:12 -0700

    Improved webpack config support.

--- apps/hello-browser/webpack.config.js
+++ apps/hello-browser/webpack.config.js
@@ -12,14 +12,14 @@ tswpConfig.staticServeDir = [path.resolve(__dirname, 'static')];
 const config = {
 	entry: path.join(__dirname, 'src/main.ts'),
 	output: {
-		path: path.resolve(path.join('build', 'hello-browser'))
+		path: path.resolve('build', 'hello-browser')
 	}
 };
 
-const envConfig = process.env.NODE_ENV === 'production' ? require(path.join('scripts', 'wpc-prod')) : require(path.join('scripts', 'wpc-dev'));
+const envConfig = process.env.NODE_ENV === 'production' ? require(path.resolve('scripts', 'wpc-prod')) : require(path.resolve('scripts', 'wpc-dev'));
 module.exports = merge(
-	require(path.join('scripts', 'wpc-common')),
-	require(path.join('scripts', 'wpc-web')),
+	require(path.resolve('scripts', 'wpc-common')),
+	require(path.resolve('scripts', 'wpc-web')),
 	envConfig,
 	config
 );
